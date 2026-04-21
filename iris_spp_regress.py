3942ff6ce071d91e9eb180f15d976c8e6cf2f569
Hannah M. Adkins <ham0018@auburn.edu>
2026-04-21 18:29:01 -0500

    Edits to make script run as a reproducible module

--- iris_spp_regress.py
+++ iris_spp_regress.py
@@ -4,10 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
+
 def filter_species(input_file, species_column):
     """
-    Filter a data frame by species and return a list of data frames.
-    The input file must contain a column with species names for filtering.
+    Filter a data frame by species and return a dictionary of
+    species name : data frame pairs. The input file must contain a column with
+    species names for filtering.
 
      Parameters
     ----------
@@ -19,10 +21,10 @@ def filter_species(input_file, species_column):
 
      Returns
     -------
-    list
-        A list of data frames. Each data frame contains the information from a 
-        single species. The number of data frames in the list is equal to the 
-        number of species in the input file.
+    dict
+        A dictionary of species name : data frame pairs. Each data frame
+        contains the information from a single species. The number of pairs is
+        equal to the number of species in the input file.
     """
     dataframe = pd.read_csv(input_file)
     species = dataframe[species_column].unique()
@@ -35,10 +37,37 @@ def filter_species(input_file, species_column):
 
     return species_df_dict
 
-def species_regress(species_df_dict):
+
+def species_regress(species_df_dict, 
+                    predictor,
+                    response):
+    """
+    Performs and plots regressions for specified predictor and response
+    variables from data frames provided in a dictionary of
+    species name : data frame pairs.
+
+     Parameters
+    ----------
+    species_df_dict : dict
+        A dictionary of species name : data frame pairs generated using the
+        filter_species function.
+
+    pred_var : str
+        The column that contains the predictor (independent) variable values.
+
+    res_var : str
+        The column that contains the response (dependent) variable values.
+
+     Returns
+    -------
+    image file
+        png image of the plotted data and regression line for a single species.
+        The number of images generated is equal the number of pairs in the dict.
+        
+    """
     for key, value in species_df_dict.items():
-        x = value.petal_length_cm
-        y = value.sepal_length_cm
+        x = value[predictor]
+        y = value[response]
         regression = stats.linregress(x, y)
         slope = regression.slope
         intercept = regression.intercept
@@ -49,7 +78,12 @@ def species_regress(species_df_dict):
         plt.legend()
         plt.savefig(f"{key}_regress.png")
         plt.close()
+        print(f"{key}_regress.png created in working directory")
+
 
 if __name__ == '__main__':
-    input_file = "iris.csv"
-    filter_column = "species"
+    species_df_dict = filter_species(input_file = "iris.csv",
+                                     filter_column = "species")
+    species_regress(species_df_dict = species_df_dict,
+                    predictor = "petal_length_cm",
+                    response = "sepal_length_cm")
